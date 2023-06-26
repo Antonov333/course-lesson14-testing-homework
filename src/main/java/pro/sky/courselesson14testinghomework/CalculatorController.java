dce6163aa0dffac1301f11dfa9150925fb7eda81
Antonov333 <antonovsergei333@yahoo.com>
2023-06-26 19:48:45 +0300

    Borrowed controller and service classes from previous homework

---
+++ src/main/java/pro/sky/courselesson14testinghomework/CalculatorController.java
@@ -0,0 +1,47 @@
+package pro.sky.courselesson14testinghomework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    private final CalculatorService calculatorService = new CalculatorService();
+
+    @GetMapping
+    public String hint() {
+        return calculatorService.hint();
+    }
+
+    @GetMapping(path = "/calculator/")
+    public String welcome() {
+        return calculatorService.welcome();
+    }
+
+    @GetMapping(path = "/calculator/plus/")
+    public String plus(@RequestParam(name = "num1", required = false) Integer userNum1,
+                       @RequestParam(name = "num2", required = false) Integer userNum2) {
+        return calculatorService.plus(userNum1, userNum2);
+    }
+
+    @GetMapping(path = "/calculator/divide/")
+    public String divide(@RequestParam(name = "num1", required = false) Integer userNum1,
+                         @RequestParam(name = "num2", required = false) Integer userNum2) {
+        return calculatorService.divide(userNum1, userNum2);
+    }
+
+    @GetMapping(path = "/calculator/minus/")
+    public String minus(@RequestParam(name = "num1", required = false) Integer num1,
+                        @RequestParam(name = "num2", required = false) Integer num2) {
+        return calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/multiply/")
+    public String multiply(@RequestParam(name = "num1", required = false) Integer num1,
+                           @RequestParam(name = "num2", required = false) Integer num2) {
+        return calculatorService.multiply(num1, num2);
+    }
+
+
+}
---
+++ src/main/java/pro/sky/courselesson14testinghomework/CalculatorService.java
@@ -0,0 +1,94 @@
+package pro.sky.courselesson14testinghomework;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public String hint() {
+        return
+                "<b>Hi! <br><br>Welcome to simple <a href=\"http://localhost:8080/calculator/\">Calculator</a>!";
+    }
+
+    public String welcome() {
+        return "<b>Welcome to simple calculator!</b><br><br>" +
+                "<a href=\"http://localhost:8080/calculator/plus/?num1=1&num2=1\"> Plus </a> | " +
+                "<a href=\"http://localhost:8080/calculator/minus/?num1=1&num2=1\"> Minus </a> | " +
+                "<a href=\"http://localhost:8080/calculator/multiply/?num1=1&num2=1\"> Multiply </a> | " +
+                "<a href=\"http://localhost:8080/calculator/divide/?num1=1&num2=1\"> Divide </a>";
+    }
+
+    public String plus(Integer num1, Integer num2) {
+        int code = checkParamsCode(num1, num2);
+        String result = "<b>Plus</b><br><br>" + checkParamsString(code);
+        if (code == 0) {
+            result = result.concat(
+                    num1 + " + " + num2 + " = " + (num1 + num2));
+        }
+        return result;
+    }
+
+
+    public String divide(Integer num1, Integer num2) {
+        int code = checkParamsCode(num1, num2);
+        String result = "<b>Division</b><br><br>" + checkParamsString(code);
+        if (code == 0 && num2.intValue() == 0) {
+            code = code + 1000;
+            result = result.concat(" -- ERROR: division by zero");
+        }
+        if (code == 0) {
+            result = result.concat(num1 + " / " + num2 + " = "
+                    + num1 / num2);
+        }
+        return result;
+    }
+
+    public String minus(Integer num1, Integer num2) {
+        int code = checkParamsCode(num1, num2);
+        String result = "<b>Minus</b><br><br>" + checkParamsString(code);
+        if (code == 0) {
+            result = result.concat(num1 + " - " + num2 + " = " + (num1 - num2));
+        }
+        return result;
+    }
+
+    public String multiply(Integer num1, Integer num2) {
+        int code = checkParamsCode(num1, num2);
+        String result = "<b>Multiply</b><br><br>" + checkParamsString(code);
+        if (code == 0) {
+            result = result.concat(
+                    num1 + " * " + num2 + " = " + num1 * num2);
+        }
+        return result;
+    }
+
+    private int checkParamsCode(Object p1, Object p2) {
+        int c = 0;
+        if (p1 == null) {
+            c = 10;
+        }
+        if (p2 == null) {
+            c += 100;
+        }
+        return c;
+        // 0 - both params are OK
+        //10 - first param is missing
+        //100 - second param is missing
+        //110 - both params are missing
+    }
+
+    private String checkParamsString(int code) {
+        switch (code) {
+            case 0:
+                return "";
+            case 10:
+                return "ERROR: first parameter is missing";
+            case 100:
+                return "ERROR: second parameter is missing";
+            case 110:
+                return "ERROR: first and second parameters are missing";
+        }
+        return "checkParamsString went wrong";
+    }
+
+}
