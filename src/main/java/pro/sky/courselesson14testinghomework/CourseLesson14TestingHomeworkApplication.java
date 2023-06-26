84f74e5e7100cda328d917cbcfec09f68fd973df
Antonov333 <antonovsergei333@yahoo.com>
2023-06-26 19:30:13 +0300

    Initial commit for Testing topic homework (Course 2, Lesson 14)

---
+++ src/main/java/pro/sky/courselesson14testinghomework/CourseLesson14TestingHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.courselesson14testinghomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseLesson14TestingHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseLesson14TestingHomeworkApplication.class, args);
+    }
+
+}
